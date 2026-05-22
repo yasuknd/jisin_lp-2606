704f42ebd9c7b663c661bad13c7c667257d1ea93
yasuknd <yasu.knd@gmail.com>
2026-05-22 11:14:40 +0900

    gift編集

--- src/components/Benefits/SeasonalGifts/SeasonalGifts.jsx
+++ src/components/Benefits/SeasonalGifts/SeasonalGifts.jsx
@@ -1,17 +1,8 @@
-import giftCooking from '../../../assets/images/gifts/gift-cooking-01.png';
-import giftImperialCalendar from '../../../assets/images/gifts/gift-imperial-calendar-01.jpg';
+import giftImperialCalendar from '../../../assets/images/gifts/gift-imperial-calendar-01.png';
 import giftImperialDigest from '../../../assets/images/gifts/gift-imperial-digest-01.png';
 import './SeasonalGifts.scss';
 
 const seasonalGifts = [
-  {
-    title: '「お料理コレクション」ポストカード',
-    description: '4号ごとに、編集部おすすめレシピをポストカードにしてお届け',
-    image: giftCooking,
-    imageAlt: '「お料理コレクション」ポストカードのイメージ',
-    imageWidth: 1251,
-    imageHeight: 743,
-  },
   {
     title: '皇室報道ダイジェスト',
     description: '1年分の皇室報道をまとめた特別冊子を、12月発売号に同梱予定',
@@ -19,6 +10,7 @@ const seasonalGifts = [
     imageAlt: '皇室報道ダイジェストのイメージ',
     imageWidth: 1850,
     imageHeight: 1864,
+    imageLayout: 'square',
   },
   {
     title: '2027年「皇室ご予定」丸わかりカレンダー',
@@ -27,6 +19,7 @@ const seasonalGifts = [
     imageAlt: '2027年「皇室ご予定」丸わかりカレンダーのイメージ',
     imageWidth: 6048,
     imageHeight: 4024,
+    imageLayout: 'landscape',
   },
 ];
 
@@ -42,26 +35,38 @@ function SeasonalGifts({ label }) {
           </p>
         </header>
         <div className="seasonalGifts__content">
-          <ul className="seasonalGifts__cards">
-            {seasonalGifts.map((gift) => (
-              <li key={gift.title} className="seasonalGifts__card">
-                <figure className="seasonalGifts__image">
-                  <img
-                    src={gift.image}
-                    alt={gift.imageAlt}
-                    width={gift.imageWidth}
-                    height={gift.imageHeight}
-                    loading="lazy"
-                    decoding="async"
-                  />
-                </figure>
-                <div className="seasonalGifts__cardBody">
-                  <h4 className="seasonalGifts__cardTitle">{gift.title}</h4>
-                  <p className="seasonalGifts__cardText">{gift.description}</p>
-                </div>
-              </li>
-            ))}
-          </ul>
+          <div className="seasonalGifts__panel">
+            <ul className="seasonalGifts__cards">
+              {seasonalGifts.map((gift) => (
+                <li
+                  key={gift.title}
+                  className={`seasonalGifts__card seasonalGifts__card--${gift.imageLayout}`}
+                >
+                  <article className="seasonalGifts__cardInner">
+                    <figure className="seasonalGifts__image">
+                      <img
+                        src={gift.image}
+                        alt={gift.imageAlt}
+                        width={gift.imageWidth}
+                        height={gift.imageHeight}
+                        loading="lazy"
+                        decoding="async"
+                      />
+                    </figure>
+                    <div className="seasonalGifts__cardBody">
+                      <h4 className="seasonalGifts__cardTitle">{gift.title}</h4>
+                      <p className="seasonalGifts__cardText">{gift.description}</p>
+                    </div>
+                  </article>
+                </li>
+              ))}
+            </ul>
+            <p className="seasonalGifts__more">
+              <span className="seasonalGifts__moreLine" aria-hidden="true" />
+              <span className="seasonalGifts__moreMark">and more</span>
+              <span className="seasonalGifts__moreLine" aria-hidden="true" />
+            </p>
+          </div>
         </div>
       </div>
     </section>
