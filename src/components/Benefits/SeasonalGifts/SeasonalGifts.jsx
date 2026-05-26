25590f5176b2d54f28113f51a30117272a61f9c8
yasuknd <yasu.knd@gmail.com>
2026-05-26 10:53:53 +0900

    gift変更

--- src/components/Benefits/SeasonalGifts/SeasonalGifts.jsx
+++ src/components/Benefits/SeasonalGifts/SeasonalGifts.jsx
@@ -1,26 +1,16 @@
-import giftImperialCalendar from '../../../assets/images/gifts/gift-imperial-calendar-01.png';
-import giftImperialDigest from '../../../assets/images/gifts/gift-imperial-digest-01.png';
+import giftCooking01 from '../../../assets/images/gifts/gift-cooking-01.png';
+import giftCooking02 from '../../../assets/images/gifts/gift-cooking-02.png';
+import giftCooking03 from '../../../assets/images/gifts/gift-cooking-03.png';
+import giftCooking04 from '../../../assets/images/gifts/gift-cooking-04.png';
+import giftCooking05 from '../../../assets/images/gifts/gift-cooking-05.png';
 import './SeasonalGifts.scss';
 
-const seasonalGifts = [
-  {
-    title: '皇室報道ダイジェスト',
-    description: '1年分の皇室報道をまとめた特別冊子を、12月発売号に同梱予定',
-    image: giftImperialDigest,
-    imageAlt: '皇室報道ダイジェストのイメージ',
-    imageWidth: 1850,
-    imageHeight: 1864,
-    imageLayout: 'square',
-  },
-  {
-    title: '2027年「皇室ご予定」丸わかりカレンダー',
-    description: '皇室のご予定がひと目でわかるカレンダーを、3月発売号に同梱予定',
-    image: giftImperialCalendar,
-    imageAlt: '2027年「皇室ご予定」丸わかりカレンダーのイメージ',
-    imageWidth: 6048,
-    imageHeight: 4024,
-    imageLayout: 'landscape',
-  },
+const cookingGiftImages = [
+  { src: giftCooking01, alt: '「お料理コレクション」ポストカードのイメージ' },
+  { src: giftCooking02, alt: '' },
+  { src: giftCooking03, alt: '' },
+  { src: giftCooking04, alt: '' },
+  { src: giftCooking05, alt: '' },
 ];
 
 function SeasonalGifts({ label }) {
@@ -36,31 +26,30 @@ function SeasonalGifts({ label }) {
         </header>
         <div className="seasonalGifts__content">
           <div className="seasonalGifts__panel">
-            <ul className="seasonalGifts__cards">
-              {seasonalGifts.map((gift) => (
-                <li
-                  key={gift.title}
-                  className={`seasonalGifts__card seasonalGifts__card--${gift.imageLayout}`}
-                >
-                  <article className="seasonalGifts__cardInner">
+            <article className="seasonalGifts__feature">
+              <ul className="seasonalGifts__gallery">
+                {cookingGiftImages.map((image) => (
+                  <li key={image.src} className="seasonalGifts__galleryItem">
                     <figure className="seasonalGifts__image">
                       <img
-                        src={gift.image}
-                        alt={gift.imageAlt}
-                        width={gift.imageWidth}
-                        height={gift.imageHeight}
+                        src={image.src}
+                        alt={image.alt}
+                        width={740}
+                        height={740}
                         loading="lazy"
                         decoding="async"
                       />
                     </figure>
-                    <div className="seasonalGifts__cardBody">
-                      <h4 className="seasonalGifts__cardTitle">{gift.title}</h4>
-                      <p className="seasonalGifts__cardText">{gift.description}</p>
-                    </div>
-                  </article>
-                </li>
-              ))}
-            </ul>
+                  </li>
+                ))}
+              </ul>
+              <div className="seasonalGifts__cardBody">
+                <h4 className="seasonalGifts__cardTitle">「お料理コレクション」ポストカード</h4>
+                <p className="seasonalGifts__cardText">
+                  4号ごとに、編集部おすすめレシピをポストカードにしてお届け
+                </p>
+              </div>
+            </article>
             <p className="seasonalGifts__more">
               <span className="seasonalGifts__moreLine" aria-hidden="true" />
               <span className="seasonalGifts__moreMark">and more</span>
